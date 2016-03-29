c7a7fa56b174167f9dab4ab5d5d239b4935c2120
Maria Levandoski <maria@localhost.localdomain>
2016-03-28 22:31:44 -0400

    Just about everything besides cookies is functional

--- template/quiz2.js
+++ template/quiz2.js
@@ -5,8 +5,9 @@
 	// Whether or not you use jQuery, you still have to fix the jQuery errors. Removing jQuery is not fixing the problem.
 
 	$mouseover = $('.mouseover');
-	$click     = $('.clikc');
-	$sub       = $('.submit');
+	$click     = $('.click');
+	$submit       = $('.submit');
+	$timeout   = $('.timeout');
 
 	$mouseover.on('mouseover', function() {
 		$this = $(this);
@@ -14,9 +15,9 @@
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
-		$this.hmtl('Peace Out!')
-		$(this).fadeout(1500);
+	$click.on('click', function() {
+		$(this).html('Peace Out!');
+		$(this).fadeOut(1500);
 		return false;
 	});
 
@@ -24,15 +25,46 @@
 		e.preventDefault();
 		if ($(this).find('input[type="text"]').val() !== '') {
 			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
+				$(this).fadeOut('slow');
 			});
-			$(this).appendwith('<h2>Congratulations! You've entered some text!</h2>');
+			$(this).appendwith('<h2>Congratulations! You\'ve entered some text!</h2>');
 		}
+	});
+
+	var $titleCookie;
+	//Get Title / Change It
+	$("#action-button").click(function(){
+		$.ajax({
+			url: "http://www.mattbowytz.com/simple_api.json?data=quizData",
+			data: {
+				format: 'json'
+			},
+			error: function() {
+				$('#title').html('<p>Error, sad day :(</p>');
+			},
+			success: function(data){
+				document.getElementById("action-button2").style.display='inline';
+				document.getElementById("action-button").innerHTML="Change It";
+				var $title = $('<h1>').text(data.data[Math.floor(Math.random()*data.data.length)]);
+				$('#title').html($title);
+				$titleCookie = $title;
+			},
+			type: 'GET'
+		});
+	});
+
+	//keep it
+	$("#action-button2").click(function(){
+		document.getElementById("action-button2").style.display='none';
+		document.getElementById("action-button").innerHTML="Get Title";
+		document.cookies="title="+$titleCookie.text();
+		$('#title').append(document.cookies.split("=")[1]);
+		//$('#title').append($titleCookie.text());
 	});
 
 	$(document).on(ready, function() {
 		setTimeout({
-			$timeout.fadeIn('slow');
+			//$timeout.fadeIn('slow');
 		}, 1000);
 	});
 
